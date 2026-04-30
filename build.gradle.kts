8f4e864800e57f3a1b8a6559a85ace2a26575a54
Kwizera Kennedy <kwizerakennedy16@gmail.com>
2026-04-30 19:05:25 +0300

    chore: Add version comment to root build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+// App version: 1.0.0 - Initial Release
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
